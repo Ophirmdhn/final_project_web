b169d191b12241e3ae441a59f5070e633a23b2bb
Ophirmdhn <ophiramadhan18@gmail.com>
2023-06-23 22:26:53 +0800

    Menambah Pesan

--- data_barang.php
+++ data_barang.php
@@ -3,41 +3,36 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>Data Barang</title>
+    <title>Document</title>
 </head>
 <body>
     <table class="table table-striped">
     <thead>
         <tr>
-        <th scope="col">#</th>
-        <th scope="col">First</th>
-        <th scope="col">Last</th>
-        <th scope="col">Handle</th>
+        <th scope="col">No</th>
+        <th scope="col">Nama Barang</th>
+        <th scope="col">Jenis Barang</th>
+        <th scope="col">Jumlah Barang</th>
+        <th scope="col">Keterangan</th>
+        <th scope="col">Waktu Pemesanan</th>
         </tr>
     </thead>
     <tbody>
+
+        <?php
+        $tampil = mysqli_query ($koneksi, "SELECT * FROM tb_barang");
+        while ($data = mysqli_fetch_array($tampil)):
+        ?>
         <tr>
-        <th scope="row">1</th>
-        <td>Mark</td>
-        <td>Otto</td>
-        <td>@mdo</td>
-        </tr>
-        <tr>
-        <th scope="row">2</th>
-        <td>Jacob</td>
-        <td>Thornton</td>
-        <td>@fat</td>
-        </tr>
-        <tr>
-        <th scope="row">3</th>
-        <td>Larry</td>
-        <td>the Bird</td>
-        <td>@twitter</td>
+            
+            <td><?php echo $data['id_barang']; ?></td>
+            <td><?php echo $data['nama_barang']; ?></td>
+            <td><?php echo $data['jenis_barang']; ?></td>
+            <td><?php echo $data['jumlah_barang']; ?></td>
+            <td><?php echo $data['keterangan']; ?></td>
+            <td><?php echo $data['waktu']; ?></td>
         </tr>
     </tbody>
-    </table>    
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+    </table> 
 </body>
 </html>
