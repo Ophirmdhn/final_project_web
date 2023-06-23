a3e383084187f743bf4e781d27b542a05afb332f
Dwi Ophi Ramadhan <106944091+Ophirmdhn@users.noreply.github.com>
2023-06-23 16:18:55 +0800

    Buat Menu Order

--- dashboard.php
+++ dashboard.php
@@ -20,10 +20,6 @@ if (!isset($_SESSION['username'])){
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        
         <a class="navbar-brand fw-bolder" href="#">Tiki - Toko</a>
         <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
@@ -35,6 +31,9 @@ if (!isset($_SESSION['username'])){
             </li>
             <li class="nav-item">
                 <a class="nav-link active mx-3" aria-current="page" href="#contact">Contact</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link active mx-3" aria-current="page" href="#order">Order</a>
             </li>
         </ul>
         <form class="d-flex" role="search">
@@ -54,23 +53,20 @@ if (!isset($_SESSION['username'])){
         </div>
         <div class="p-md-5 m-md-3 bg-body"></div>
 
-        <div class="p-md-5 m-md-0 bg-secondary"></div>
+        <div class="p-md-5 m-md-0 bg-secondary" id="about"></div>
         <div class="position-relative overflow-hidden p-3 p-md-5 m-md-0 text-center bg-secondary">
             <div class="col-md-7 p-lg-5 mx-auto my-5">
-                <h1 class="display-5 fw-bold text-white" id="about">About</h1>
+                <h1 class="display-5 fw-bold text-white">About</h1>
                 <h3 class="fw-normal text-white mb-3">
                     Tiki - Toko adalah toko yang menyediakan bahan kebutuhan anda, yang menarik dari toko ini 
                     adalah anda bisa meminta/memesan barang yang anda inginkan (Pre Order).
                 </h3>
-                <form class="" role="search">
-                     <a class="btn btn-light" href="#" role="button">Buat Pesanan</a>
-                </form>
             </div>
         </div>
         <div class="p-md-5 m-md-0 bg-secondary"></div>
 
-        <div class="p-md-5 m-md-3 bg-body"></div>
-        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body" id="team">
+        <div class="p-md-5 m-md-3 bg-body" id="team"></div>
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body">
             <div class="p-lg-5 mx-auto my-auto">
                 <h1 class="display-5 fw-bold">Team</h1>
                 <h3 class="fw-normal text-muted mb-3">Utiwi</h3>
@@ -78,8 +74,8 @@ if (!isset($_SESSION['username'])){
         </div>
         <div class="p-md-5 m-md-3 bg-body"></div>
 
-        <div class="p-md-5 m-md-0 bg-secondary"></div>
-        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-0 text-center bg-secondary" id="contact">
+        <div class="p-md-5 m-md-0 bg-secondary" id="contact"></div>
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-0 text-center bg-secondary">
             <div class="col-md-7 p-lg-5 mx-auto my-5">
                 <h1 class="display-5 fw-bold text-white">Contact</h1>
                 <h3 class="fw-normal text-white mb-3">
@@ -88,6 +84,23 @@ if (!isset($_SESSION['username'])){
             </div>
         </div>
         <div class="p-md-5 m-md-0 bg-secondary"></div>
+
+        <div class="p-md-5 m-md-3 bg-body" id="order"></div>
+        <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-body">
+            <div class="col-md-9 p-lg-5 mx-auto my-auto">
+                <h1 class="display-5 fw-bold">Order</h1>
+                <h3 class="fw-normal text-muted mb-3">
+                    Kami akan menyediakan barang yang anda inginkan!
+                    silahka pilih menu di bawah ini untuk melakukan pemesanan.
+                </h3>
+                <form class="" role="search">
+                     <a class="btn btn-dark" href="#" role="button">Buat Pesanan</a>
+                     <a class="btn btn-dark" href="#" role="button">Pesanan Saya</a>
+                </form>
+            </div>
+        </div>
+        <div class="p-md-5 m-md-3 bg-body"></div>
+
     </main>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
